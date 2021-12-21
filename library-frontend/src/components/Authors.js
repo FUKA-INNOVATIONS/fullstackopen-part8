d13c9208973d318d0b1e390ba99598b1a99dc987
fuka-innovations <fuad.kalhori@gmail.com>
2021-12-21 19:17:00 +0200

    8.11

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,6 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from '../queries';
+import React, { useEffect, useState } from 'react'
+import { useMutation, useQuery } from '@apollo/client';
+import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK, EDIT_AUTHOR } from '../queries';
+
+const EditAuthor = () => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+  const [error, setError] = useState('')
+
+  const [ editAuthor, result ] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [ { query: ALL_BOOKS }, { query: ALL_AUTHORS } ],
+    onError: (error) => {
+      setError(error.graphQLErrors[0].message)
+    }
+  })
+
+  const submit = async (event) => {
+    event.preventDefault()
+    editAuthor({ variables: { name: name, setBornTo: born } })
+
+    setName('')
+    setBorn('')
+
+    setInterval(() => {
+      setError('')
+    }, 3000)
+  }
+
+  useEffect(() => {
+    if (result.data && result.data.editAuthor === null) {
+      setError('Author not found')
+    }
+  }, [result.data, setError])
+
+  return (
+      <>
+        <form onSubmit={submit}>
+          <h3>Set birthyear </h3>
+          <div>
+            name
+            <input
+                value={name}
+                onChange={({ target }) => setName(target.value)}
+            />
+          </div>
+          <div>
+            born
+            <input
+                type='number'
+                value={born}
+                onChange={({ target }) => setBorn(Number(target.value))}
+            />
+          </div>
+          <button type='submit'>Update author</button>
+        </form>
+        <p style={{color: 'red'}}>{error}</p>
+      </>
+  )
+}
 
 const Authors = (props) => {
   const result = useQuery(ALL_AUTHORS)
@@ -43,6 +99,10 @@ const Authors = (props) => {
           )}
         </tbody>
       </table>
+      <br />
+      <div>
+        <EditAuthor />
+      </div>
 
     </div>
   )
